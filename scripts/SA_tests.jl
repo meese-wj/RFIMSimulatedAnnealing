658dba9b27e628fca1cbb80bffd83ce0bdc023de
W. Joe Meese <meese022@umn.edu>
2022-09-20 15:16:05 -0500

    Annealing runs, but now to figure out allocations...

--- scripts/SA_tests.jl
+++ scripts/SA_tests.jl
@@ -1,15 +1,19 @@
 using DrWatson; @quickactivate :RFIMSimulatedAnnealing
+using RFIMSimulatedAnnealing.MonteCarloMethods: AbstractModel
 
 struct BaseIsingModel{T <: AbstractFloat} <: AbstractModel
     lattice::CubicLattice2D
     hamiltonian::BasicIsingHamiltonian{T}
 
     BaseIsingModel(args...) = BaseIsingModel{Float64}(args...)
-    function BasicIsingModel{T}(; Lx = 16, Ly = Lx, Jex = 1) where T
+    function BaseIsingModel{T}(Lx = 16, Ly = Lx, Jex = 1) where T
         latt = CubicLattice2D(Int(Lx), Int(Ly))
-        ham = BasicIsingHamiltonian{T}(latt, BasicIsingParameters{T}(T(Jex)))
+        ham = BasicIsingHamiltonian(latt, BasicIsingParameters{T}(T(Jex)))
         return new{T}(latt, ham)
     end
 end
 
-ising_model = BaseIsingModel()
+ising_model = BaseIsingModel(4)
+# sa_params = SimulatedAnnealingParameters(2^10, 1, Float64[3, 2.75, 2.5, 2.25, 2, 1.75, 1.5])
+sa_params = SimulatedAnnealingParameters(2^5, 1, Float64[3])
+@time anneal!(ising_model, sa_params, metropolis_sweep!, false)
--- src/MonteCarloMethods/SimulatedAnnealing.jl
+++ src/MonteCarloMethods/SimulatedAnnealing.jl
@@ -1,6 +1,21 @@
 
 import ..Hamiltonians: energy, write_state
-export anneal!, SA_datapath
+export anneal!, SA_datapath, SimulatedAnnealingParameters
+
+struct SimulatedAnnealingParameters{T <: Float64} <: AbstractMonteCarloParameters
+    therm_sweeps::Int        # Total number of sweeps while thermalizing at a given temperature
+    # measure_sweeps::Int    # Total number of sweeps used to take measurements
+    total_measurements::Int  # Total number of measurements
+    temperatures::Vector{T}
+
+    SimulatedAnnealingParameters(args...) = SimulatedAnnealingParameters{Float64}(args...)
+    SimulatedAnnealingParameters{T}(args...) where T = new{T}(args...)
+end
+thermalization_sweeps(params::SimulatedAnnealingParameters) = params.therm_sweeps
+sampling_sweeps(params::SimulatedAnnealingParameters) = zero(Int)
+total_measurements(params::SimulatedAnnealingParameters) = params.total_measurements
+sweeps_per_export(params::SimulatedAnnealingParameters) = thermalization_sweeps(params)
+temperatures(params::SimulatedAnnealingParameters) = params.temperatures
 
 function SA_datapath(model::AbstractModel, temperature)
     return "NDoFs=$(num_DoF(model))_T=$temperature"
@@ -8,7 +23,8 @@ end
 
 SA_datapath(pathprepend::String = "", args...) = pathprepend * SA_datapath(args...)
 
-function anneal!(model::AbstractModel, temperature_regimen, mc_params::AbstractMonteCarloParameters, mc_sweep::Function, write_out = true, pathprepend = "")
+function anneal!(model::AbstractModel, mc_params::AbstractMonteCarloParameters, mc_sweep::Function, write_out = true, pathprepend = "")
+    temperature_regimen = temperatures(mc_params)
     spe = sweeps_per_export(mc_params)
     @inbounds for temperature ∈ temperature_regimen
         beta = 1 / temperature
@@ -18,6 +34,6 @@ function anneal!(model::AbstractModel, temperature_regimen, mc_params::AbstractM
             thermalize!(model, beta, mc_params, mc_sweep)
         end
         # end
-        write_out ? write_state(Hamiltonian(model), SA_datapath(pathprepend, model)) : nothing
+        write_out ? write_state(Hamiltonian(model), SA_datapath(pathprepend, model, temperature)) : nothing
     end
 end

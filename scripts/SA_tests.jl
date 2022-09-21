1c64c68ab5a65af6a99342b24f6eee66e97f79e8
W. Joe Meese <meese022@umn.edu>
2022-09-21 18:32:19 -0500

    Adaptive code seems to be working. Now to check the regimen

--- scripts/SA_tests.jl
+++ scripts/SA_tests.jl
@@ -15,5 +15,5 @@ end
 
 ising_model = BaseIsingModel(64)
 # sa_params = SimulatedAnnealingParameters(2^10, 1, Float64[3, 2.75, 2.5, 2.25, 2, 1.75, 1.5])
-sa_params = SimulatedAnnealingParameters(2^16, 1, Float64[1])
+sa_params = SimulatedAnnealingParameters(10, 2^10, 0.05, Float64[8])
 @time anneal!(ising_model, sa_params, metropolis_sweep!, false)

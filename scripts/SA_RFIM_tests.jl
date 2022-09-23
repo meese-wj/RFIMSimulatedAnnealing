57b1428ee702722afdcd45a44ccf6437c84b2737
W. Joe Meese <meese022@umn.edu>
2022-09-22 19:37:48 -0500

    Got the RFIM running. Now for tests

---
+++ scripts/SA_RFIM_tests.jl
@@ -0,0 +1,21 @@
+using DrWatson; @quickactivate :RFIMSimulatedAnnealing
+using RFIMSimulatedAnnealing.MonteCarloMethods: AbstractModel
+
+struct RandomFieldIsingModel{T <: AbstractFloat} <: AbstractModel
+    lattice::CubicLattice2D
+    hamiltonian::RandomFieldIsingHamiltonian{T}
+
+    function RandomFieldIsingModel{T}(Lx = 16, Ly = Lx, Jex = 1, hext = 0, Δh = 0) where T
+        latt = CubicLattice2D(Int(Lx), Int(Ly))
+        ham = RandomFieldIsingHamiltonian( latt, RandomFieldIsingParameters{T}(T(Jex), T(hext), T(Δh)) )
+        return new{T}(latt, ham)
+    end
+    RandomFieldIsingModel(args...) = RandomFieldIsingModel{Float64}(args...)
+end
+
+ising_model = RandomFieldIsingModel(128, 128, 1, 0, 0.9)
+Tregimen = Float64[8, 7, 6, 5, 4.5, 4, 3.5, 3, 2.5, 2.375, 2.325, 2.275, 2.25, 2.20, 2.15, 2.125, 2.0, 1.75, 1.5, 1.25, 1.0, 0.75, 0.5, 0.1]
+# Tregimen = Float64[3]
+# sa_params = SimulatedAnnealingParameters(2^10, 1, Float64[3, 2.75, 2.5, 2.25, 2, 1.75, 1.5])
+sa_params = SimulatedAnnealingParameters(10, 2^11, 0.01, Tregimen)
+@time anneal!(ising_model, sa_params, metropolis_sweep!, true, datadir())
--- src/Hamiltonians/IsingHamiltonians/ConcreteIsingHamiltonians.jl
+++ src/Hamiltonians/IsingHamiltonians/ConcreteIsingHamiltonians.jl
@@ -1,11 +1,18 @@
 # Main code for concrete Ising Hamiltonians
+using Random, Distributions
 
-export BasicIsingHamiltonian, BasicIsingParameters
+export BasicIsingHamiltonian, BasicIsingParameters, RandomFieldIsingHamiltonian, RandomFieldIsingParameters
 
 struct BasicIsingParameters{T <: AbstractFloat} <: AbstractIsingParameters
     Jex::T  # Ising exchanges. Jex > 0 is ferromagnetic
 end
 BasicIsingParameters{T}(; J::T = 1. ) where {T <: AbstractFloat} = BasicIsingParameters{T}( J )
+
+struct RandomFieldIsingParameters{T <: AbstractFloat} <: AbstractIsingParameters
+    Jex::T   # Ising exchanges. Jex > 0 is ferromagnetic
+    hext::T  # External field. Spins should be colinear with hext by Zeeman energy
+    Δh::T    # Characteristic width of the random field distribution.
+end
 
 mutable struct BasicIsingHamiltonian{T <: AbstractFloat} <: AbstractIsing
     params::BasicIsingParameters{T}
@@ -17,10 +24,39 @@ mutable struct BasicIsingHamiltonian{T <: AbstractFloat} <: AbstractIsing
     end
 end
 
+function random_field_generator(Δh, ndofs)
+    Δh == zero(Δh) ? zeros(typeof(Δh), ndofs) : nothing
+    nrng = Normal(zero(Δh), Δh)
+    return rand(nrng, ndofs)
+end
+
+mutable struct RandomFieldIsingHamiltonian{T <: AbstractFloat} <: AbstractZeemanIsing
+    params::RandomFieldIsingParameters{T}
+    spins::Vector{T}
+    random_fields::Vector{T}
+
+    function RandomFieldIsingHamiltonian(latt, params::RandomFieldIsingParameters{T}) where T
+        ndofs = num_sites(latt)
+        return new{T}(params, rand([one(T), -one(T)], ndofs), random_field_generator(params.Δh, ndofs))
+    end
+end
+
+@inline Zeeman_field(ham::RandomFieldIsingHamiltonian, site) = ham.params.hext + ham.random_fields[site]
+
 @inline site_energy(ham::BasicIsingHamiltonian, latt, site, site_values) = _base_site_energy(ham, latt, site, site_values)
+@inline site_energy(ham::RandomFieldIsingHamiltonian, latt, site, site_value) = _base_site_energy(ham, latt, site, site_value) + _Zeeman_site_energy(ham, site, site_value)
 
 @inline energy(ham::BasicIsingHamiltonian, latt) = _base_energy(ham, latt)
+function energy(ham::RandomFieldIsingHamiltonian, latt)
+    en = _base_energy(ham, latt)
+    @inbounds for (site, dof_location_val) ∈ enumerate(IterateBySite, ham)
+        en += _Zeeman_site_energy(ham, dof_location_val[1], dof_location_val[2])
+    end
+    return en
+end
 
 @inline DoF_energy_change(ham::BasicIsingHamiltonian, latt, site) = _base_DoF_energy_change(ham, latt, site)
+@inline DoF_energy_change(ham::RandomFieldIsingHamiltonian, latt, site) = _base_DoF_energy_change(ham, latt, site) + _Zeeman_DoF_energy_change(ham, site)
 
 @inline accept_move!(condition::Bool, ham::BasicIsingHamiltonian, site) = _base_accept_move!(condition, ham, site)
+@inline accept_move!(condition::Bool, ham::RandomFieldIsingHamiltonian, site) = _base_accept_move!(condition, ham, site)
